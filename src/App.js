512495b82511c27e013e8e918d7dabbbd03d376a
Brian Cao <98575161+CaoBeee@users.noreply.github.com>
2023-03-08 10:31:14 -0800

    require http link image form

--- src/App.js
+++ src/App.js
@@ -84,32 +84,34 @@ class App extends Component {
 
 	onPictureSubmit = () => {
 		this.setState({ imageUrl: this.state.input })
-		fetch('https://smart-brain-api-caobeee.onrender.com/imageurl', {
-			method: 'POST',
-			headers: { 'Content-Type': 'application/json' },
-			body: JSON.stringify({
-				input: this.state.input,
-			}),
-		})
-			.then(response => response.json())
-			.then(response => {
-				if (response) {
-					fetch('https://smart-brain-api-caobeee.onrender.com/image', {
-						method: 'PUT',
-						headers: { 'Content-Type': 'application/json' },
-						body: JSON.stringify({
-							id: this.state.user.id,
-						}),
-					})
-						.then(response => response.json())
-						.then(count => {
-							this.setState(Object.assign(this.state.user, { entries: count }))
-						})
-						.catch(console.log)
-				}
-				this.displayFaceBox(this.calculateFaceLocation(response))
+		if (this.state.input !== '' && this.state.input.includes('http')) {
+			fetch('https://smart-brain-api-caobeee.onrender.com/imageurl', {
+				method: 'POST',
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify({
+					input: this.state.input,
+				}),
 			})
-			.catch(err => console.log(err))
+				.then(response => response.json())
+				.then(response => {
+					if (response) {
+						fetch('https://smart-brain-api-caobeee.onrender.com/image', {
+							method: 'PUT',
+							headers: { 'Content-Type': 'application/json' },
+							body: JSON.stringify({
+								id: this.state.user.id,
+							}),
+						})
+							.then(response => response.json())
+							.then(count => {
+								this.setState(Object.assign(this.state.user, { entries: count }))
+							})
+							.catch(console.log)
+					}
+					this.displayFaceBox(this.calculateFaceLocation(response))
+				})
+				.catch(err => console.log(err))
+		}
 	}
 
 	onRouteChange = route => {
